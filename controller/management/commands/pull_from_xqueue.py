d62833cdaa919c41441bdcceb7d12ae3aaf3a187
Vik Paruchuri <vik@edx.org>
2012-11-12 10:34:14 -0500

    Add in logic to catch errors, and define area in which to post to grading controller

--- controller/management/commands/pull_from_xqueue.py
+++ controller/management/commands/pull_from_xqueue.py
@@ -27,8 +27,14 @@ class Command(BaseCommand):
 
         while flag:
             for queue_name in args:
-                queue_item=self.get_from_queue(queue_name)
-                log.debug(queue_item)
+                try:
+                    queue_item=self.get_from_queue(queue_name)
+                    log.debug(parse_xreply(queue_item))
+                    
+                    #Post to grading controller here!
+
+                except Exception as err:
+                    log.debug("Error getting submission: ".format(err))
                 time.sleep(2)
 
     def login(self):
@@ -74,35 +80,6 @@ class Command(BaseCommand):
 
         return parse_xreply(r.text)
 
-
-    def push_orphaned_submissions(self, orphaned_submissions):
-        for orphaned_submission in orphaned_submissions:
-            current_time = timezone.now()
-            time_difference = (current_time - orphaned_submission.arrival_time).total_seconds()
-            if time_difference > settings.ORPHANED_SUBMISSION_TIMEOUT:
-
-                log.info("Found orphaned submission: queue_name: {0}, lms_header: {1}".format(
-                    orphaned_submission.queue_name, orphaned_submission.xqueue_header))
-                orphaned_submission.num_failures += 1
-
-                payload = {'xqueue_body': orphaned_submission.xqueue_body,
-                           'xqueue_files': orphaned_submission.s3_urls}
-
-                orphaned_submission.grader_id = settings.XQUEUES[orphaned_submission.queue_name]
-                orphaned_submission.push_time = timezone.now()
-                (grading_success, grader_reply) = _http_post(orphaned_submission.grader_id, json.dumps(payload), settings.GRADING_TIMEOUT)
-                orphaned_submission.return_time = timezone.now()
-
-                if grading_success:
-                    orphaned_submission.grader_reply = grader_reply
-                    orphaned_submission.lms_ack = post_grade_to_lms(orphaned_submission.xqueue_header, grader_reply)
-                else:
-                    log.error("Submission {} to grader {} failure: Reply: {}, ".format(orphaned_submission.id, orphaned_submission.grader_id, grader_reply))
-                    orphaned_submission.num_failures += 1
-                    orphaned_submission.lms_ack = post_failure_to_lms(orphaned_submission.xqueue_header)
-
-                orphaned_submission.retired = True # NOTE: Retiring pushed submissions after one shot regardless of grading_success
-                orphaned_submission.save()
 
 def parse_xreply(xreply):
     """
