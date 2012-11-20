029e0659bd954beaf75f7e16effd032626c8a2a0
Victor Shnayder <victor@edx.org>
2012-11-20 18:23:25 -0500

    Add a few comments

--- controller/models.py
+++ controller/models.py
@@ -23,25 +23,34 @@ STATE_CODES = (
 CHARFIELD_LEN_SMALL = 128
 
 class Submission(models.Model):
+    # controller state
     next_grader_type=models.CharField(max_length=2, choices=GRADER_TYPE,default="NA")
     previous_grader_type=models.CharField(max_length=2, choices=GRADER_TYPE, default="NA")
+    state = models.CharField(max_length=1, choices= STATE_CODES)
+    grader_settings=models.TextField(default="")
 
-    prompt = models.TextField(default="")
-    rubric=models.TextField(default="")
+    # data about the submission
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+    prompt = models.TextField(default="")
+    rubric = models.TextField(default="")
     student_id = models.CharField(max_length=CHARFIELD_LEN_SMALL)
+
+    # specified in the input type--can be reused between many different
+    # problems.  (Should perhaps be named something like problem_type)
     problem_id = models.CharField(max_length=CHARFIELD_LEN_SMALL)
+
+    # passed by the LMS
+    location = models.CharField(max_length=CHARFIELD_LEN_SMALL, default="")
+    max_score = models.IntegerField(default=1)
     course_id = models.CharField(max_length=CHARFIELD_LEN_SMALL)
-    state = models.CharField(max_length=1, choices= STATE_CODES)
     student_response = models.TextField(default="")
-    student_submission_time=models.DateTimeField(default=datetime.datetime.now)
-    xqueue_submission_id=models.CharField(max_length=CHARFIELD_LEN_SMALL,default="")
-    xqueue_submission_key=models.CharField(max_length=CHARFIELD_LEN_SMALL,default="")
+    student_submission_time = models.DateTimeField(default=datetime.datetime.now)
+
+    # xqueue details
+    xqueue_submission_id = models.CharField(max_length=CHARFIELD_LEN_SMALL,default="")
+    xqueue_submission_key = models.CharField(max_length=CHARFIELD_LEN_SMALL,default="")
     xqueue_queue_name = models.CharField(max_length=CHARFIELD_LEN_SMALL,default="")
-    location=models.CharField(max_length=CHARFIELD_LEN_SMALL,default="")
-    max_score=models.IntegerField(default=1)
-    grader_settings=models.TextField(default="")
 
     def __unicode__(self):
         sub_row = "Essay to be graded from student {0}, in course {1}, and problem {2}.  ".format(
@@ -74,6 +83,7 @@ class Submission(models.Model):
         )
         return successful_peer_graders
 
+# TODO: what's a better name for this?  GraderResult?
 class Grader(models.Model):
     submission = models.ForeignKey('Submission')
     score=models.IntegerField()
@@ -81,8 +91,14 @@ class Grader(models.Model):
     status_code = models.CharField(max_length=1,choices=STATUS_CODES)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified= models.DateTimeField(auto_now=True)
+
+    # For human grading, this is the id of the user that graded the submission.
+    # For machine grading, it's the name and version of the algorithm that was
+    # used.
     grader_id=models.CharField(max_length=CHARFIELD_LEN_SMALL)
     grader_type=models.CharField(max_length=2, choices=GRADER_TYPE)
+
+    # should be between 0 and 1, with 1 being most confident.
     confidence=models.DecimalField(max_digits=10, decimal_places=9)
 
 
