326c4e6639684a98967d836ac8a35f47151fde8c
Vik Paruchuri <vik@edx.org>
2012-11-21 14:16:53 -0500

    Added calibration history/calibration record model to store calibration data

--- peer_grading/models.py
+++ peer_grading/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+CHARFIELD_LEN_SMALL = 128
+class CalibrationHistory(models.Model):
+    student_id=models.CharField(max_length=CHARFIELD_LEN_SMALL)
+
+    #Have problem_id and location in order to allow for one to be user_defined, and one system defined
+    #This allows for the same problem to be used across classes without re-calibration if needed.
+    #Currently use location instead of problem_id
+    problem_id=models.CharField(max_length=CHARFIELD_LEN_SMALL)
+    location=models.CharField(max_length=CHARFIELD_LEN_SMALL)
+
+    def __unicode__(self):
+        history_row=("Calibration history for student {0} on problem {1} at location {2}").format(
+            self.student_id,self.problem_id,self.location)
+        return history_row
+
+    def get_all_calibration_records(self):
+        return self.calibrationrecord_set.all()
+
+    def get_calibration_record_count(self):
+        return self.get_all_calibration_records().count()
+
+    def get_average_calibration_error(self):
+        all_records=list(self.get_all_calibration_records())
+        errors=[abs(all_records[i].actual_score-all_records[i].score) for i in xrange(0,len(all_records))]
+        total_error=0
+        for i in xrange(0,len(errors)):
+            total_error+=errors[i]
+        average_error=total_error/float(len(errors))
+        return average_error
+
+class CalibrationRecord(models.Model):
+    calibration_history=models.ForeignKey("CalibrationHistory")
+    score=models.IntegerField()
+    actual_score=models.IntegerField()
+
+    #This tracks whether the record was created from a calibration essay prior to the student starting grading,
+    #Or from a calibration essay inserted into the peer grading
+    is_pre_calibration=models.BooleanField(default=True)
