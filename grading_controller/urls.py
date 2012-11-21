e7709b3d902ff5c492e91f8d007b3e5dfa2b596f
Vik Paruchuri <vik@edx.org>
2012-11-21 12:51:12 -0500

    Fix some strangeness associated with urls.py after merging branches.  Add log to gitignore.

--- grading_controller/urls.py
+++ grading_controller/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^grading_controller/', include('controller.urls')),
-<<<<<<< HEAD
     url(r'^peer_grading/', include('peer_grading.urls')),
-=======
     url(r'^staff_grading/', include('staff_grading.urls')),
->>>>>>> origin/master
 )
