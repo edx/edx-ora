87185762955fe9397b7b9085aebada5307b160f1
Vik Paruchuri <vik@edx.org>
2012-11-21 09:24:08 -0500

    Added basic peer grading framework.

--- grading_controller/urls.py
+++ grading_controller/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^grading_controller/', include('controller.urls')),
+    url(r'^peer_grading/', include('peer_grading.urls')),
 )
