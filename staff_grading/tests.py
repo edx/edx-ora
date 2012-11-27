dbe302c6f3479300f7f229c58f3f134c985f2c36
Vik Paruchuri <vik@edx.org>
2012-11-27 18:26:27 -0500

    Start on staff grading util, fix bug in staff grading views that arose when functions were moved out of util.py

--- staff_grading/tests.py
+++ staff_grading/tests.py
@@ -1,16 +1,52 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+Run me with:
+    python manage.py test --settings=grading_controller.settings staff_grading
 """
+import json
+import unittest
+from datetime import datetime
+import logging
+import urlparse
 
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test.client import Client
+import requests
+import test_util
+from django.conf import settings
+from controller.models import Submission, SubmissionState, Grader, GraderStatus
+from peer_grading.models import CalibrationHistory,CalibrationRecord
+from django.utils import timezone
+
+log = logging.getLogger(__name__)
+
+GET_NEXT="/staff_grading/get_next_submission/"
+SAVE_GRADE="/staff_grading/save_grade/"
+
+LOCATION="MITx/6.002x"
+STUDENT_ID="5"
+COURSE_ID="course_id"
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class StaffGradingViewTest(unittest.TestCase):
+    def setUp(self):
+        test_util.create_user()
+
+        self.c = Client()
+        response = self.c.login(username='test', password='CambridgeMA')
+
+    def tearDown(self):
+        test_util.delete_all()
+
+    def test_get_next_submission_false(self):
+        content = self.c.get(
+            GET_NEXT,
+            data={'course_id' : COURSE_ID, "grader_id" : STUDENT_ID},
+        )
+
+        body = json.loads(content.content)
+
+        #Should return true, but with message of no submissions to grade
+        self.assertEqual(body['message'], "No more submissions to grade.")
+        self.assertEqual(body['success'], True)
+
+
