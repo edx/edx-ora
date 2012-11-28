80a36bd898e641d22557d75b188957d29b057b20
Vik Paruchuri <vik@edx.org>
2012-11-28 15:45:25 -0500

    Fix some issues, add comments.

--- controller/grader_util.py
+++ controller/grader_util.py
@@ -16,9 +16,9 @@ def create_grader(grader_dict,sub):
         grader_id=grader_dict['grader_id'],
         grader_type=grader_dict['grader_type'],
         confidence=grader_dict['confidence'],
+        submission=sub,
     )
 
-    grade.submission = sub
     grade.save()
 
     return grade
