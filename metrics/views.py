a418a4edc8fa8dc69f5a8e99cf3220c486522091
Vik Paruchuri <vik@edx.org>
2012-11-28 20:08:44 -0500

    Implement timing, get simple stub view for it

--- metrics/views.py
+++ metrics/views.py
@@ -15,6 +15,7 @@ from reportlab.graphics.charts.barcharts import HorizontalBarChart
 
 from models import Timing
 
+@csrf_exempt
 def query_metrics(request):
     """
     Request is an HTTP get request with the following keys:
@@ -23,44 +24,49 @@ def query_metrics(request):
         Location
     """
 
-    if request.method != "GET":
-        raise Http404
+    if request.method == "POST":
+        course_id = request.POST.get('course_id')
+        grader_type = request.POST.get('grader_type')
+        location = request.POST.get('location')
 
-    course_id = request.GET.get('course_id')
-    grader_type = request.GET.get('grader_type')
-    location = request.GET.get('location')
+        query_dict = {
+            'course_id' : course_id,
+            'grader_type' : grader_type,
+            'location' : location
+        }
+        arguments = {}
+        for k, v in query_dict.items():
+            if v:
+                arguments[k] = v
 
-    query_dict = {
-        'course_id' : course_id,
-        'grader_type' : grader_type,
-        'location' : location
-    }
-    arguments = {}
-    for k, v in query_dict.items():
-        if v:
-            arguments[k] = v
+        timing_set=Timing.objects.filter(**arguments)
+        timing_set_values=timing_set.values("start_time", "end_time")
+        timing_set_start=[i['start_time'] for i in timing_set_values]
+        timing_set_end=[i['end_time'] for i in timing_set_values]
+        timing_set_difference=[(timing_set_end[i]-timing_set_start[i]).total_seconds() for i in xrange(0,len(timing_set_end))]
+        d = BarChartDrawing()
 
-    timing_set=Timing.objects.filter(**arguments)
-    timing_set_values=timing_set.values("start_time", "end_time")
-    timing_set_start=[i['start_time'] for i in timing_set_values]
-    timing_set_end=[i['end_time'] for i in timing_set_values]
-    timing_set_difference=[timing_set_end[i]-timing_set_start[i] for i in xrange(0,len(timing_set_end))]
+        d.chart.data = [timing_set_difference]
+        binary_char = d.asString("gif")
+        response=HttpResponse(binary_char, 'image/gif')
+        #response['Content-Disposition'] = 'attachment; filename=output.gif'
+        return response
 
-
-    # Our query is ready to take off.
-    film_results = Timing.objects.filter(
-        director__name=request_params['director'],
-        created_at__range=(fromdate, todate)
-    )
-    return render_to_response('search_results.html', {'results':film_results})
+    elif request.method == "GET":
+        url_base = settings.GRADING_CONTROLLER_INTERFACE['url']
+        if not url_base.endswith("/"):
+            url_base += "/"
+        rendered=render_to_string('metrics_display.html',
+            {'ajax_url' : url_base})
+        return HttpResponse(rendered)
 
 
-class MyBarChartDrawing(Drawing):
+class BarChartDrawing(Drawing):
     def __init__(self, width=400, height=200, *args, **kw):
         Drawing.__init__(self,width,height,*args,**kw)
         self.add(HorizontalBarChart(), name='chart')
 
-        self.add(String(200,180,'Hello World'), name='title')
+        self.add(String(200,180,'Timing Data for Request'), name='title')
 
         #set any shapes, fonts, colors you want here.  We'll just
         #set a title font and place the chart within the drawing
