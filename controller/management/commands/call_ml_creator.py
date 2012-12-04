9102be78c9ec97b80b7c9e2832ea79a22714e38f
Vik Paruchuri <vik@edx.org>
2012-12-04 15:26:36 -0500

    Add in code to (hopefully) fix some transaction handling issue with manage.py commands

--- controller/management/commands/call_ml_creator.py
+++ controller/management/commands/call_ml_creator.py
@@ -2,6 +2,11 @@ from django.conf import settings
 from django.core.management.base import NoArgsCommand
 from django.utils import timezone
 
+#from http://jamesmckay.net/2009/03/django-custom-managepy-commands-not-committing-transactions/
+#Fix issue where db data in manage.py commands is not refreshed at all once they start running
+from django.db import transaction
+transaction.commit_unless_managed()
+
 import requests
 import urlparse
 import time
--- controller/management/commands/call_ml_grader.py
+++ controller/management/commands/call_ml_grader.py
@@ -1,6 +1,11 @@
 from django.conf import settings
 from django.core.management.base import NoArgsCommand
 
+#from http://jamesmckay.net/2009/03/django-custom-managepy-commands-not-committing-transactions/
+#Fix issue where db data in manage.py commands is not refreshed at all once they start running
+from django.db import transaction
+transaction.commit_unless_managed()
+
 from django.utils import timezone
 import requests
 import urlparse
