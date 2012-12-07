9936228ae0eeb3ddfea8ecfda6a2a6f65660ad2b
Vik Paruchuri <vik@edx.org>
2012-12-07 10:59:57 -0500

    Reformat code

--- basic_check/basic_check_util.py
+++ basic_check/basic_check_util.py
@@ -17,13 +17,14 @@ def perform_spelling_and_grammar_checks(string):
     Output:
         Feedback dictionary and an essay set object
     """
-    feature_ext=feature_extractor.FeatureExtractor()
-    e_set=essay_set.EssaySet(type="test")
-    e_set.add_essay(string,0)
+    feature_ext = feature_extractor.FeatureExtractor()
+    e_set = essay_set.EssaySet(type="test")
+    e_set.add_essay(string, 0)
 
-    feedback=feature_ext.gen_feedback(e_set)[0]
+    feedback = feature_ext.gen_feedback(e_set)[0]
 
     return feedback, e_set
+
 
 def simple_quality_check(string):
     """
@@ -38,28 +39,28 @@ def simple_quality_check(string):
 
     #Maximum proportion of characters in a string that can be badly spelled or grammatically incorrect
     #before it is rejected
-    SPELLING_MAXIMUM=.3
-    GRAMMAR_MAXIMUM=.1
+    SPELLING_MAXIMUM = .3
+    GRAMMAR_MAXIMUM = .1
 
     #Minimum number of characters needed in a string before it is ok
-    LENGTH_MINIMUM=10
+    LENGTH_MINIMUM = 10
 
     #Minimum characters per word needed in a response (below is rejected)
-    CHARS_PER_WORD_MINIMUM=3
+    CHARS_PER_WORD_MINIMUM = 3
 
-    quality_dict={'feedback' : {}, 'score' : 1, 'grader_type' : 'BC', 'status' : GraderStatus.success}
+    quality_dict = {'feedback': {}, 'score': 1, 'grader_type': 'BC', 'status': GraderStatus.success}
     try:
-        basic_check, e_set=perform_spelling_and_grammar_checks(string)
-        total_length=len(string)
-        word_length_ratio=total_length/float(len(e_set._tokens[0]))
+        basic_check, e_set = perform_spelling_and_grammar_checks(string)
+        total_length = len(string)
+        word_length_ratio = total_length / float(len(e_set._tokens[0]))
     except:
-        quality_dict['status']=GraderStatus.failure
+        quality_dict['status'] = GraderStatus.failure
         return False, quality_dict
 
-    quality_dict['feedback']=json.dumps({ k: basic_check[k] for k in ['markup_text','spelling','grammar'] })
-    if(total_length<LENGTH_MINIMUM or word_length_ratio<=CHARS_PER_WORD_MINIMUM or
-       basic_check['grammar_per_char']>GRAMMAR_MAXIMUM or basic_check['spelling_per_char']>SPELLING_MAXIMUM):
-        quality_dict['score']=0
+    quality_dict['feedback'] = json.dumps({k: basic_check[k] for k in ['markup_text', 'spelling', 'grammar']})
+    if(total_length < LENGTH_MINIMUM or word_length_ratio <= CHARS_PER_WORD_MINIMUM or
+       basic_check['grammar_per_char'] > GRAMMAR_MAXIMUM or basic_check['spelling_per_char'] > SPELLING_MAXIMUM):
+        quality_dict['score'] = 0
 
     return True, quality_dict
 
