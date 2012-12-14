049cbccf2b362cb0b0db0ccbe9301225fa59ece7
Vik Paruchuri <vik@edx.org>
2012-12-14 14:51:56 -0500

    Minor fixes

--- controller/create_grader.py
+++ controller/create_grader.py
@@ -1,6 +1,10 @@
 from models import Grader
+import logging
+
+log=logging.getLogger(__name__)
 
 def create_grader(grader_dict, sub):
+    log.debug("Creating grader with feedback: {0} and type {1}".format(grader_dict['feedback'], grader_dict['grader_type']))
     grade = Grader(
         score=grader_dict['score'],
         feedback=grader_dict['feedback'],
