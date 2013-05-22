60f1ef45fad90e5dace6c8c222f978c28fce9cfa
Vik Paruchuri <vik.paruchuri@gmail.com>
2013-05-22 17:46:48 -0400

    Start to  move manage.py commands over to celery tasks

---
+++ controller/tasks.py
@@ -0,0 +1,146 @@
+from django.conf import settings
+
+#from http://jamesmckay.net/2009/03/django-custom-managepy-commands-not-committing-transactions/
+#Fix issue where db data in manage.py commands is not refreshed at all once they start running
+from django.db import transaction
+
+import time
+import logging
+from statsd import statsd
+import random
+from django import db
+from controller.pull_from_xqueue_util import check_for_completed_submissions, post_one_submission_back_to_queue, pull_from_single_grading_queue
+
+import controller.util as util
+from controller.models import Submission
+import controller.expire_submissions as expire_submissions
+from metrics import generate_student_metrics
+import gc
+from statsd import statsd
+import project_urls
+
+from celery.task import periodic_task, task
+
+log = logging.getLogger(__name__)
+
+@periodic_task(run_every=settings.TIME_BETWEEN_EXPIRED_CHECKS)
+def expire_submissions():
+    flag = True
+    log.debug("Starting check for expired subs.")
+    while flag:
+        try:
+            gc.collect()
+            db.reset_queries()
+            transaction.commit_unless_managed()
+            subs = Submission.objects.all()
+
+            #Comment out submission expiration for now.  Not really needed while testing.
+            expire_submissions.reset_timed_out_submissions(subs)
+            """
+            expired_list = expire_submissions.get_submissions_that_have_expired(subs)
+            if len(expired_list) > 0:
+                success = expire_submissions.finalize_expired_submissions(expired_list)
+                statsd.increment("open_ended_assessment.grading_controller.remove_expired_subs",
+                    tags=["success:{0}".format(success)])
+            """
+            try:
+                expire_submissions.reset_in_subs_to_ml(subs)
+                transaction.commit_unless_managed()
+            except:
+                log.exception("Could not reset in to ml!")
+            try:
+                expire_submissions.reset_subs_in_basic_check(subs)
+                transaction.commit_unless_managed()
+            except:
+                log.exception("Could reset subs in basic check!")
+
+            try:
+                expire_submissions.reset_failed_subs_in_basic_check(subs)
+                transaction.commit_unless_managed()
+            except:
+                log.exception("Could not reset failed subs in basic check!")
+
+            try:
+                expire_submissions.reset_ml_subs_to_in()
+                transaction.commit_unless_managed()
+            except:
+                log.exception("Could not reset ml to in!")
+
+            try:
+                #See if duplicate peer grading items have been finished grading
+                expire_submissions.add_in_duplicate_ids()
+                transaction.commit_unless_managed()
+            except:
+                log.exception("Could not finish checking for duplicate ids!")
+
+            try:
+                #See if duplicate peer grading items have been finished grading
+                expire_submissions.check_if_grading_finished_for_duplicates()
+                transaction.commit_unless_managed()
+            except:
+                log.exception("Could not finish checking if duplicates are graded!")
+
+            try:
+                #Mark submissions as duplicates if needed
+                expire_submissions.mark_student_duplicate_submissions()
+                transaction.commit_unless_managed()
+            except:
+                log.exception("Could not mark subs as duplicate!")
+
+            try:
+                generate_student_metrics.regenerate_student_data()
+                transaction.commit_unless_managed()
+            except:
+                log.exception("Could not regenerate student data!")
+
+            try:
+                #Remove old ML grading models
+                expire_submissions.remove_old_model_files()
+                transaction.commit_unless_managed()
+            except:
+                log.exception("Could not remove ml grading models!")
+
+            log.debug("Finished looping through.")
+
+        except Exception as err:
+            log.exception("Could not get submissions to expire! Error: {0}".format(err))
+            statsd.increment("open_ended_assessment.grading_controller.remove_expired_subs",
+                             tags=["success:Exception"])
+            transaction.commit_unless_managed()
+
+
+
+@periodic_task(run_every=settings.TIME_BETWEEN_XQUEUE_PULLS)
+def pull_from_xqueue():
+    """
+    Constant loop that pulls from queue and posts to grading controller
+    """
+    log.info(' [*] Pulling from xqueues...')
+
+    #Define sessions for logging into xqueue and controller
+    xqueue_session = util.xqueue_login()
+    controller_session = util.controller_login()
+    #Login, then setup endless query loop
+    flag = True
+
+    while flag:
+        #Loop through each queue that is given in arguments
+        for queue_name in settings.GRADING_QUEUES_TO_PULL_FROM:
+            #Check for new submissions on xqueue, and send to controller
+            pull_from_single_grading_queue(queue_name,controller_session,xqueue_session, project_urls.ControllerURLs.submit, project_urls.ControllerURLs.status)
+
+        #Loop through message queues to see if there are any messages
+        for queue_name in settings.MESSAGE_QUEUES_TO_PULL_FROM:
+            pull_from_single_grading_queue(queue_name,controller_session,xqueue_session, project_urls.ControllerURLs.submit_message, project_urls.ControllerURLs.status)
+
+        #Sleep for some time to allow other pull_from_xqueue processes to get behind/ahead
+        time_sleep_value = random.uniform(0, .1)
+        time.sleep(time_sleep_value)
+
+        #Check for finalized results from controller, and post back to xqueue
+        transaction.commit_unless_managed()
+
+        submissions_to_post = check_for_completed_submissions()
+        for submission in list(submissions_to_post):
+            post_one_submission_back_to_queue(submission, xqueue_session)
+        transaction.commit_unless_managed()
--- metrics/metrics_util.py
+++ metrics/metrics_util.py
@@ -7,108 +7,12 @@ from metrics.models import Timing, FIELDS_TO_EVALUATE, StudentCourseProfile
 from controller.models import  Submission, SubmissionState, Grader, GraderStatus, Message
 import logging
 import matplotlib.pyplot as plt
-import StringIO
 from matplotlib import numpy as np
-import re
-import csv
-import numpy
-from django.forms.models import model_to_dict
+from metrics import tasks
 
 log = logging.getLogger(__name__)
 
 IMAGE_ERROR_MESSAGE = "Error processing image."
-
-def sub_commas(text):
-    fixed_text=re.sub(","," ",text)
-    return fixed_text
-
-def encode_ascii(text):
-    return text.encode('ascii', 'ignore')
-
-def set_up_data_dump(locations,name):
-    fixed_name=re.sub("[/:]","_",name)
-
-    if isinstance(locations, basestring):
-        locations=[locations]
-
-    response = HttpResponse(mimetype='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="{0}.csv"'.format(fixed_name)
-    writer = csv.writer(response)
-
-    return writer, locations, response
-
-def join_if_list(text):
-    if isinstance(text,list):
-        text=" ".join(text)
-    return text
-
-
-def get_data_in_csv_format(locations, name):
-    writer, locations, response = set_up_data_dump(locations, name)
-
-    for z in xrange(0,len(locations)):
-        location=locations[z]
-        fixed_location=re.sub("[/:]","_",location)
-
-        subs=Submission.objects.filter(location=location,state=SubmissionState.finished)
-        grader_info=[sub.get_all_successful_scores_and_feedback() for sub in subs]
-        grader_type=[grade['grader_type'] for grade in grader_info]
-        score=[numpy.median(grade['score']) for grade in grader_info]
-        feedback=[sub_commas(encode_ascii(join_if_list(grade['feedback']))) for grade in grader_info]
-        success=[grade['success'] for grade in grader_info]
-        submission_text=[sub_commas(encode_ascii(sub.student_response)) for sub in subs]
-        max_score=[sub.max_score for sub in subs]
-
-        if z==0:
-            writer.writerow(["Score", "Max Score","Grader Type", "Success", "Submission Text", "Location"])
-        for i in xrange(0,len(grader_info)):
-            writer.writerow([score[i], max_score[i], grader_type[i], success[i], submission_text[i], location])
-
-    return True, response
-
-def get_message_in_csv_format(locations, name):
-    writer, locations, response = set_up_data_dump(locations, name)
-
-    for z in xrange(0,len(locations)):
-        location=locations[z]
-        fixed_location=re.sub("[/:]","_",location)
-
-        messages=Message.objects.filter(grader__submission__location=location)
-        message_score=[message.score for message in messages]
-        message_text=[sub_commas(encode_ascii(message.message)) for message in messages]
-
-        if z==0:
-            writer.writerow(["Message Text", "Score", "Location"])
-        for i in xrange(0,len(message_score)):
-            writer.writerow([message_text[i], message_score[i], location])
-
-    return True, response
-
-def get_student_data_in_csv_format(locations, name):
-    writer, locations, response = set_up_data_dump(locations, name)
-
-    for z in xrange(0,len(locations)):
-        location=locations[z]
-        fixed_location=re.sub("[/:]","_",location)
-
-        student_course_profiles=StudentCourseProfile.objects.filter(course_id=location)
-        student_course_profiles_count = student_course_profiles.count()
-
-        if z==0:
-            writer.writerow(FIELDS_TO_EVALUATE)
-        for i in xrange(0,student_course_profiles_count):
-            field_values = []
-            all_zeros = True
-            scp_dict = model_to_dict(student_course_profiles[i])
-            for m in xrange(0,len(FIELDS_TO_EVALUATE)):
-                scp_val = scp_dict.get(FIELDS_TO_EVALUATE[m], 0)
-                field_values.append(scp_val)
-                if scp_val!=0:
-                    all_zeros = False
-            if not all_zeros:
-                writer.writerow(field_values)
-
-    return True, response
 
 def generate_student_data_per_course(arguments):
     """
@@ -488,7 +392,7 @@ for field in FIELDS_TO_EVALUATE:
     AVAILABLE_METRICS.update({field : generate_student_data_per_course})
 
 AVAILABLE_DATA_DUMPS={
-    'message_dump' : get_message_in_csv_format,
-    'data_dump' : get_data_in_csv_format,
-    'student_data_dump' : get_student_data_in_csv_format,
+    'message_dump' : tasks.get_message_in_csv_format,
+    'data_dump' : tasks.get_data_in_csv_format,
+    'student_data_dump' : tasks.get_student_data_in_csv_format,
 }
