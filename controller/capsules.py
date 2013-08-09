4f5a0cfc8fc3197ebc6ce663e973f729415e3662
Vik Paruchuri <vik@edx.org>
2013-08-09 13:53:37 -0400

    Centralize capsules (for peer refactor)

---
+++ controller/capsules.py
@@ -0,0 +1,44 @@
+from models import Submission, SubmissionState
+
+class LocationCapsule(object):
+    """
+    Ecapsulates information that graders may want about a location.
+    """
+    def __init__(self, location):
+        self.location = location
+
+    @property
+    def location_submissions(self):
+        """
+        Gets all submissions for a particular location.
+        """
+        return Submission.objects.filter(location=self.location)
+
+    @property
+    def all_pending(self):
+        """
+        Gets all submissions for the location that are waiting to be graded.
+        """
+        return self.location_submissions.filter(state=SubmissionState.waiting_to_be_graded)
+
+    @property
+    def all_pending_count(self):
+        """
+        Counts all pending submissions.
+        """
+        return self.all_pending.count()
+
+class CourseCapsule(object):
+    """
+    Encapsulates information that graders may want about a course.
+    """
+    def __init__(self, course_id):
+        self.course_id = course_id
+
+    @property
+    def locations(self):
+        """
+        Gets all locations in a course.
+        """
+        return [x['location'] for x in
+                list(Submission.objects.filter(course_id=self.course_id).values('location').distinct())]
