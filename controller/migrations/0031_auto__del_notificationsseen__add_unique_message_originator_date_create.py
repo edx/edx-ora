f8f16360793b0d16a467cbfec4add8e4029e1909
Vik Paruchuri <vik@edx.org>
2013-09-13 14:22:28 -0400

    Remove text fields from constraints

--- controller/migrations/0031_auto__del_notificationsseen__add_unique_message_originator_date_create.py
+++ controller/migrations/0031_auto__del_notificationsseen__add_unique_message_originator_date_create.py
@@ -11,19 +11,19 @@ class Migration(SchemaMigration):
         # Deleting model 'NotificationsSeen'
         db.delete_table('controller_notificationsseen')
 
-        # Adding unique constraint on 'Message', fields ['originator', 'message', 'grader']
-        db.create_unique('controller_message', ['originator', 'message', 'grader_id'])
+        # Adding unique constraint on 'Message', fields ['originator', 'date_created', 'grader']
+        db.create_unique('controller_message', ['originator', 'date_created', 'grader_id'])
 
-        # Adding unique constraint on 'Submission', fields ['xqueue_submission_id', 'student_response', 'location', 'student_id']
-        db.create_unique('controller_submission', ['xqueue_submission_id', 'student_response', 'location', 'student_id'])
+        # Adding unique constraint on 'Submission', fields ['xqueue_submission_id', 'student_id', 'location']
+        db.create_unique('controller_submission', ['xqueue_submission_id', 'student_id', 'location'])
 
 
     def backwards(self, orm):
-        # Removing unique constraint on 'Submission', fields ['xqueue_submission_id', 'student_response', 'location', 'student_id']
-        db.delete_unique('controller_submission', ['xqueue_submission_id', 'student_response', 'location', 'student_id'])
+        # Removing unique constraint on 'Submission', fields ['xqueue_submission_id', 'student_id', 'location']
+        db.delete_unique('controller_submission', ['xqueue_submission_id', 'student_id', 'location'])
 
-        # Removing unique constraint on 'Message', fields ['originator', 'message', 'grader']
-        db.delete_unique('controller_message', ['originator', 'message', 'grader_id'])
+        # Removing unique constraint on 'Message', fields ['originator', 'date_created', 'grader']
+        db.delete_unique('controller_message', ['originator', 'date_created', 'grader_id'])
 
         # Adding model 'NotificationsSeen'
         db.create_table('controller_notificationsseen', (
@@ -45,7 +45,7 @@ class Migration(SchemaMigration):
             'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'date_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
             'feedback': ('django.db.models.fields.TextField', [], {}),
-            'grader_id': ('django.db.models.fields.CharField', [], {'default': "'1'", 'max_length': '1024'}),
+            'grader_id': ('django.db.models.fields.CharField', [], {'default': "u'1'", 'max_length': '1024'}),
             'grader_type': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_calibration': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
@@ -54,7 +54,7 @@ class Migration(SchemaMigration):
             'submission': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['controller.Submission']"})
         },
         'controller.message': {
-            'Meta': {'unique_together': "(('grader', 'message', 'originator'),)", 'object_name': 'Message'},
+            'Meta': {'unique_together': "((u'grader', u'date_created', u'originator'),)", 'object_name': 'Message'},
             'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'date_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
             'grader': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['controller.Grader']"}),
@@ -76,7 +76,7 @@ class Migration(SchemaMigration):
         },
         'controller.rubricitem': {
             'Meta': {'object_name': 'RubricItem'},
-            'comment': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'comment': ('django.db.models.fields.TextField', [], {'default': "u''"}),
             'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'date_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
             'finished_scoring': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
@@ -85,7 +85,7 @@ class Migration(SchemaMigration):
             'max_score': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
             'rubric': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['controller.Rubric']"}),
             'score': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '10', 'decimal_places': '2'}),
-            'short_text': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '1024'}),
+            'short_text': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '1024'}),
             'text': ('django.db.models.fields.TextField', [], {})
         },
         'controller.rubricoption': {
@@ -94,40 +94,40 @@ class Migration(SchemaMigration):
             'item_number': ('django.db.models.fields.IntegerField', [], {}),
             'points': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '10', 'decimal_places': '2'}),
             'rubric_item': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['controller.RubricItem']"}),
-            'short_text': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '128'}),
+            'short_text': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '128'}),
             'text': ('django.db.models.fields.TextField', [], {})
         },
         'controller.submission': {
-            'Meta': {'unique_together': "(('student_response', 'student_id', 'location', 'xqueue_submission_id'),)", 'object_name': 'Submission'},
-            'answer': ('django.db.models.fields.TextField', [], {'default': "''"}),
-            'control_fields': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'Meta': {'unique_together': "((u'student_id', u'location', u'xqueue_submission_id'),)", 'object_name': 'Submission'},
+            'answer': ('django.db.models.fields.TextField', [], {'default': "u''"}),
+            'control_fields': ('django.db.models.fields.TextField', [], {'default': "u''"}),
             'course_id': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
             'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'date_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
             'duplicate_submission_id': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'grader_settings': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'grader_settings': ('django.db.models.fields.TextField', [], {'default': "u''"}),
             'has_been_duplicate_checked': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'initial_display': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'initial_display': ('django.db.models.fields.TextField', [], {'default': "u''"}),
             'is_duplicate': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_plagiarized': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'location': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '128', 'db_index': 'True'}),
+            'location': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '128', 'db_index': 'True'}),
             'max_score': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
-            'next_grader_type': ('django.db.models.fields.CharField', [], {'default': "'NA'", 'max_length': '2'}),
+            'next_grader_type': ('django.db.models.fields.CharField', [], {'default': "u'NA'", 'max_length': '2'}),
             'posted_results_back_to_queue': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'preferred_grader_type': ('django.db.models.fields.CharField', [], {'default': "'NA'", 'max_length': '2'}),
-            'previous_grader_type': ('django.db.models.fields.CharField', [], {'default': "'NA'", 'max_length': '2'}),
+            'preferred_grader_type': ('django.db.models.fields.CharField', [], {'default': "u'NA'", 'max_length': '2'}),
+            'previous_grader_type': ('django.db.models.fields.CharField', [], {'default': "u'NA'", 'max_length': '2'}),
             'problem_id': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
-            'prompt': ('django.db.models.fields.TextField', [], {'default': "''"}),
-            'rubric': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'prompt': ('django.db.models.fields.TextField', [], {'default': "u''"}),
+            'rubric': ('django.db.models.fields.TextField', [], {'default': "u''"}),
             'skip_basic_checks': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'state': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
             'student_id': ('django.db.models.fields.CharField', [], {'max_length': '128', 'db_index': 'True'}),
-            'student_response': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'student_response': ('django.db.models.fields.TextField', [], {'default': "u''"}),
             'student_submission_time': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'xqueue_queue_name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '128'}),
-            'xqueue_submission_id': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '1024'}),
-            'xqueue_submission_key': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '1024'})
+            'xqueue_queue_name': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '128'}),
+            'xqueue_submission_id': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '1024'}),
+            'xqueue_submission_key': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '1024'})
         }
     }
 
