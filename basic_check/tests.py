73b7a9a3cd383befcaed7008a8b479d190a263fb
Stephen Sanchez <steve@edx.org>
2013-12-20 13:26:43 -0500

    Fully disabling spelling and grammar checkings for basic check.
    Ease would allow a number of keyboard mashing strings to pass spell check, but would fail a large number of submissions that were properly written Chinese.  Ease spelling and grammar check relies on a set of ascii characters, and will likely fail a large number of unicode submissions. Until spelling and grammar can properly handle multiple languages, or identify languages for each submission, it should be disabled so non-English courses may pass Basic Check.

--- basic_check/tests.py
+++ basic_check/tests.py
@@ -1,16 +1,43 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+import basic_check_util
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class BasicCheckTest(TestCase):
+    """
+    While Ease-based Basic Check has been removed, we do want to validate that some simple scenarios
+    will be returned back to the user:
+    1) If the initial text has not changed
+    2) If the response contains nothing but whitespace
+    3) Validate that the user is not banned.
+    """
+
+    _INITIAL_DISPLAY = "How much wood could a woodchuck chuck if a woodchuck could chuck wood"
+    _GOOD_STUDENT_ID = 42
+    _BANNED_STUDENT_ID = 43
+    _SKIP_BASIC_CHECKS = False  # No longer used. Should be removed on master.
+
+    def test_initial_text_has_not_changed(self):
+        success, results = basic_check_util.simple_quality_check(self._INITIAL_DISPLAY, self._INITIAL_DISPLAY,
+                                                                 self._GOOD_STUDENT_ID, self._SKIP_BASIC_CHECKS)
+        self.assertTrue(success)
+        self.assertEqual(results['score'], 0, "Score should be zero since response equals initial display.")
+
+    def test_whitespace_response(self):
+        success, results = basic_check_util.simple_quality_check("        ", self._INITIAL_DISPLAY,
+                                                                 self._GOOD_STUDENT_ID, self._SKIP_BASIC_CHECKS)
+        self.assertTrue(success)
+        self.assertEqual(results['score'], 0, "Score should be zero since response is all whitespace.")
+
+    def test_good_essay(self):
+        success, results = basic_check_util.simple_quality_check("This is a perfectly acceptable response.",
+                                                                 self._INITIAL_DISPLAY, self._GOOD_STUDENT_ID,
+                                                                 self._SKIP_BASIC_CHECKS)
+        self.assertTrue(success)
+        self.assertEqual(results['score'], 1, "Score should be 1 since response is fine.")
+
+    def test_banned_student(self):
+        success, results = basic_check_util.simple_quality_check("This is a perfectly acceptable response.",
+                                                                 self._INITIAL_DISPLAY, self._BANNED_STUDENT_ID,
+                                                                 self._SKIP_BASIC_CHECKS)
+        self.assertTrue(success)
+        self.assertEqual(results['score'], 0, "Score should be zero since student is banned.")
