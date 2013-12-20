77f4b9f734389acb1aa65a67f3b89fdc7e3da786
Stephen Sanchez <steve@edx.org>
2013-12-20 15:05:06 -0500

    Adding a unicode test and updating parens

--- basic_check/tests.py
+++ basic_check/tests.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django.test import TestCase
 import basic_check_util
 
@@ -16,20 +17,34 @@ class BasicCheckTest(TestCase):
     _SKIP_BASIC_CHECKS = False  # No longer used. Should be removed on master.
 
     def test_initial_text_has_not_changed(self):
-        success, results = basic_check_util.simple_quality_check(self._INITIAL_DISPLAY, self._INITIAL_DISPLAY,
-                                                                 self._GOOD_STUDENT_ID, self._SKIP_BASIC_CHECKS)
+        success, results = basic_check_util.simple_quality_check(
+            self._INITIAL_DISPLAY, self._INITIAL_DISPLAY,
+            self._GOOD_STUDENT_ID, self._SKIP_BASIC_CHECKS
+        )
         self.assertTrue(success)
         self.assertEqual(results['score'], 0, "Score should be zero since response equals initial display.")
 
     def test_whitespace_response(self):
-        success, results = basic_check_util.simple_quality_check("        ", self._INITIAL_DISPLAY,
-                                                                 self._GOOD_STUDENT_ID, self._SKIP_BASIC_CHECKS)
+        success, results = basic_check_util.simple_quality_check(
+            "        ", self._INITIAL_DISPLAY, self._GOOD_STUDENT_ID,
+            self._SKIP_BASIC_CHECKS
+        )
         self.assertTrue(success)
         self.assertEqual(results['score'], 0, "Score should be zero since response is all whitespace.")
 
     def test_good_essay(self):
-        success, results = basic_check_util.simple_quality_check("This is a perfectly acceptable response.",
-                                                                 self._INITIAL_DISPLAY, self._GOOD_STUDENT_ID,
-                                                                 self._SKIP_BASIC_CHECKS)
+        success, results = basic_check_util.simple_quality_check(
+            "This is a perfectly acceptable response.", self._INITIAL_DISPLAY,
+            self._GOOD_STUDENT_ID, self._SKIP_BASIC_CHECKS
+        )
+        self.assertTrue(success)
+        self.assertEqual(results['score'], 1, "Score should be 1 since response is fine.")
+
+    def test_unicode_essay(self):
+        success, results = basic_check_util.simple_quality_check(
+            u"\u6709\u65f6\u5019\uff0c\u4f60\u5403\u4e86\u5427\uff0c\u6709\u65f6\u5019\uff0c\u9152\u5427\u5403\u4f60\u3002"
+            , self._INITIAL_DISPLAY,
+            self._GOOD_STUDENT_ID, self._SKIP_BASIC_CHECKS
+        )
         self.assertTrue(success)
         self.assertEqual(results['score'], 1, "Score should be 1 since response is fine.")
