60f335f6ff23c8ddeede1174012debed9dded2af
Stephen Sanchez <steve@edx.org>
2013-12-20 13:46:19 -0500

    Remove bad unit test

--- basic_check/tests.py
+++ basic_check/tests.py
@@ -13,7 +13,6 @@ class BasicCheckTest(TestCase):
 
     _INITIAL_DISPLAY = "How much wood could a woodchuck chuck if a woodchuck could chuck wood"
     _GOOD_STUDENT_ID = 42
-    _BANNED_STUDENT_ID = 43
     _SKIP_BASIC_CHECKS = False  # No longer used. Should be removed on master.
 
     def test_initial_text_has_not_changed(self):
@@ -34,10 +33,3 @@ class BasicCheckTest(TestCase):
                                                                  self._SKIP_BASIC_CHECKS)
         self.assertTrue(success)
         self.assertEqual(results['score'], 1, "Score should be 1 since response is fine.")
-
-    def test_banned_student(self):
-        success, results = basic_check_util.simple_quality_check("This is a perfectly acceptable response.",
-                                                                 self._INITIAL_DISPLAY, self._BANNED_STUDENT_ID,
-                                                                 self._SKIP_BASIC_CHECKS)
-        self.assertTrue(success)
-        self.assertEqual(results['score'], 0, "Score should be zero since student is banned.")
